37828e003d90e937b7bd7acca43bc82e96f7bf9f
Nelson <tomneo2004@gmail.com>
2019-09-05 18:30:41 +0800

    Implement collection view

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.contentInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
     }
+    
+}
 
-
+extension ViewController : UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 13
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! CollectionViewCell
+        
+        let imageIndex = indexPath.item % 10 + 1
+        let image = UIImage(named: "img_\(imageIndex)")
+        
+        cell.imageView.image = image
+        
+        return cell
+        
+    }
+    
+    
 }
 
